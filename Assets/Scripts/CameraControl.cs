7de9b868dd58b4f5db0c04e3e4c5c3b6c8cb8a4a
bighead <longming_xu@126.com>
2016-07-18 19:06:48 +0800

    提交

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+
+    private const float lowPassFilterFactor = 0.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Input.gyro.enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.rotation = Quaternion.Slerp(transform.rotation, Input.gyro.attitude, lowPassFilterFactor);
+
+        Vector3 euler = new Vector3(0, Input.gyro.attitude.eulerAngles.y, 0);
+        this.transform.eulerAngles = euler;
+    }
+
+    void OnGUI()
+    {
+        //GUI.Label(new Rect(30, 30, 500, 20), string.Format("{0},{1},{2}", Input.gyro.attitude.x, Input.gyro.attitude.y, Input.gyro.attitude.z));
+        GUI.Label(new Rect(30, 60, 500, 20), string.Format("{0},{1},{2}", Input.gyro.attitude.eulerAngles.x, Input.gyro.attitude.eulerAngles.y, Input.gyro.attitude.eulerAngles.z));
+    }
+}
---
+++ Assets/Scripts/testEuler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testEuler : MonoBehaviour
+{
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(transform.eulerAngles);
+        Debug.Log(string.Format("{0},{1},{2}", transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z));
+
+        // 设置为无旋转（rotation是一个四元数）
+        transform.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 欧拉角：欧拉角的使用只能整体赋值，不能单独修改某一轴，详情看Unity说明
+        xRotation += Input.GetAxis("Vertical");
+        yRotation += Input.GetAxis("Horizontal");
+        Debug.Log(xRotation + "," + yRotation);
+        transform.eulerAngles = new Vector3(xRotation, yRotation, 0);
+        Debug.Log(transform.eulerAngles);
+
+        // 四元数
+        //transform.rotation = new Quaternion();
+    }
+}
